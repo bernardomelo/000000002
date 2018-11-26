86622c5169943fab80c8953ac6ca0b7ae434c453
Bernardo Gomes de Melo <bgm@cin.ufpe.br>
2018-11-26 16:55:52 -0200

    changes
    changes

--- Teste2/Teste2/spiders/example.py
+++ Teste2/Teste2/spiders/example.py
@@ -14,27 +14,50 @@ class TesteItem(scrapy.Item):
     pass
 
 
+
 class MLSpider(Spider):
     name = 'mlivre'
-    start_urls = ['https://celulares.mercadolivre.com.br/celular']
+    start_urls = ['https://lista.mercadolivre.com.br/geladeira']
 
     def parse(self, response):
-        resposta = response.xpath('//ol/div')
-        print(resposta)
+        resposta = response.xpath('//ol/li')
         items = []
         for elemento in resposta:
 
-            item = TesteItem()
-            item['Imagem'] = ' '.join(elemento.xpath('//div[contains(@class, "image-content")]/a/@href').extract())
-            item['Nome'] = ' '.join(elemento.xpath("//div/h2/a/span/text() " ).extract())
-            item['Price'] = ' '.join(elemento.xpath("//div/div/span[@class='price__fraction']/text()" ).extract())
-            item['Desconto'] = ' '.join(elemento.xpath("//div[contains(@class, 'item__discount')]/text()").extract())
-            item['Vendedor'] = ' '.join(elemento.xpath("//div[contains(@class,'item__brand')]//span/text()").extract())
-            item['ItensVendidos'] = ' '.join(elemento.xpath("//div[contains(@class,'item__condition')]/text()").extract())
-            item['InfoAdicional'] = ' '.join(elemento.xpath("//p[contains(@class, 'stack-item-info')]/text()").extract())
-            yield item
-            items.append(item)
+         
+            Imagem = ' '.join(elemento.xpath('.//div[contains(@class, "image-content")]/a/@href').extract())
+            Nome = ' '.join(elemento.xpath(".//div/h2/a/span/text() " ).extract())
+            Price = ' '.join(elemento.xpath(".//div/div/span[@class='price__fraction']/text()" ).extract())
+            Desconto = ' '.join(elemento.xpath(".//div[contains(@class, 'item__discount')]/text()").extract())
+            Vendedor = ' '.join(elemento.xpath(".//div[contains(@class,'item__brand')]//span/text()").extract())
+            ItensVendidos = ' '.join(elemento.xpath(".//div[contains(@class,'item__condition')]/text()").extract())
+            InfoAdicional = ' '.join(elemento.xpath(".//p[contains(@class, 'stack-item-info')]/text()").extract())
+            items.append(Imagem)
+            items.append(Nome)
+            items.append(Price)
+            items.append(Desconto)
+            items.append(Vendedor)
+            items.append(ItensVendidos)
+            items.append(InfoAdicional)
+
             
         pass
-        print(items)
+        while len(items) > 1:
+            print('Imagem: ', items[0])
+            print('Nome: ', items[1])
+            print('Price: ', items[2])
+            print('Desconto: ', items[3])
+            print('Vendedor: ', items[4])
+            print('ItensVendidos: ', items[5])
+            print('InfoAdicional: ', items[6])
+            print('\n')
+            items.pop(0)
+            items.pop(0)
+            items.pop(0)
+            items.pop(0)
+            items.pop(0)
+            items.pop(0)
+            items.pop(0)
+            pass
+
 
